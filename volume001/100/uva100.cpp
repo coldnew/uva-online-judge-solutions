25b1f7680f3aa44feba25444b0b77817140efa73
Yen-Chin, Lee <coldnew.tw@gmail.com>
2022-03-07 18:15:08 +0800

    uva100: add my result
    Signed-off-by: Yen-Chin, Lee <coldnew.tw@gmail.com>

---
+++ volume001/100/uva100.cpp
@@ -0,0 +1,52 @@
+#include <cstdint>
+#include <iostream>
+
+namespace {
+
+const int kMaxSize = 1000000;
+
+uint32_t CycleLength(uint32_t n) {
+  static uint32_t cache[kMaxSize] = {0};
+
+  if (n == 1)
+    return n;
+
+  if (n & 1)  // odd
+    n = 3 * n + 1;
+  else
+    n /= 2;
+
+  if (n < kMaxSize) {
+    if (!cache[n])
+      cache[n] = CycleLength(n);
+    return 1 + cache[n];
+  }
+
+  return 1 + CycleLength(n);
+}
+
+void Solve(uint32_t first, uint32_t second) {
+  // make sure start is less then end
+  int start = std::min(first, second);
+  int end = std::max(first, second);
+
+  uint32_t result = 0;
+  for (uint32_t i = start; i <= end; i++) {
+    uint32_t steps = CycleLength(i);
+    if (steps > result)
+      result = steps;
+  }
+
+  std::cout << first << " " << second << " " << result << std::endl;
+}
+
+}  // namespace
+
+#if !defined(GTEST_TEST)
+int main(int argc, char* argv[]) {
+  int first = 0, second = 0;
+  while (std::cin >> first >> second)
+    Solve(first, second);
+  return 0;
+}
+#endif  // !defind(GTEST_TEST)
---
+++ volume001/100/uva100_unittest.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+#include "uva100.cpp"
+
+namespace {
+
+std::string SolveByCaptureStdout(uint32_t start, uint32_t end) {
+  testing::internal::CaptureStdout();
+  Solve(start, end);
+  return testing::internal::GetCapturedStdout();
+}
+
+}  // namespace
+
+TEST(UVa100Test, Solver) {
+  EXPECT_STREQ(SolveByCaptureStdout(1, 10).c_str(), "1 10 20\n");
+  EXPECT_STREQ(SolveByCaptureStdout(100, 200).c_str(), "100 200 125\n");
+  EXPECT_STREQ(SolveByCaptureStdout(201, 210).c_str(), "201 210 89\n");
+  EXPECT_STREQ(SolveByCaptureStdout(900, 1000).c_str(), "900 1000 174\n");
+}
