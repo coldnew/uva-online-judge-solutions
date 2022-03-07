6b29d258703a3d2f65201993f20d1bb87cd7545b
Yen-Chin, Lee <coldnew.tw@gmail.com>
2022-03-07 16:37:49 +0800

    uva263: use c++11 raw string literals for multline test
    Signed-off-by: Yen-Chin, Lee <coldnew.tw@gmail.com>

--- volume002/263/uva263_unittest.cpp
+++ volume002/263/uva263_unittest.cpp
@@ -27,59 +27,69 @@ namespace {
 std::string SolveByCaptureStdout(uint32_t n) {
   testing::internal::CaptureStdout();
   Solve(n);
-  return testing::internal::GetCapturedStdout();
+  return "\n" + testing::internal::GetCapturedStdout();
 }
 
 }  // namespace
 
 TEST(UVa263Test, Solver) {
-  EXPECT_STREQ(SolveByCaptureStdout(123456789).c_str(),
-               "Original number was 123456789\n"
-               "987654321 - 123456789 = 864197532\n"
-               "987654321 - 123456789 = 864197532\n"
-               "Chain length 2\n\n");
+  EXPECT_STREQ(SolveByCaptureStdout(123456789).c_str(), R"(
+Original number was 123456789
+987654321 - 123456789 = 864197532
+987654321 - 123456789 = 864197532
+Chain length 2
 
-  EXPECT_STREQ(SolveByCaptureStdout(1234).c_str(),
-               "Original number was 1234\n"
-               "4321 - 1234 = 3087\n"
-               "8730 - 378 = 8352\n"
-               "8532 - 2358 = 6174\n"
-               "7641 - 1467 = 6174\n"
-               "Chain length 4\n\n");
+)");
 
-  EXPECT_STREQ(SolveByCaptureStdout(444).c_str(),
-               "Original number was 444\n"
-               "444 - 444 = 0\n"
-               "0 - 0 = 0\n"
-               "Chain length 2\n\n");
+  EXPECT_STREQ(SolveByCaptureStdout(1234).c_str(), R"(
+Original number was 1234
+4321 - 1234 = 3087
+8730 - 378 = 8352
+8532 - 2358 = 6174
+7641 - 1467 = 6174
+Chain length 4
 
-  EXPECT_STREQ(SolveByCaptureStdout(87543).c_str(),
-               "Original number was 87543\n"
-               "87543 - 34578 = 52965\n"
-               "96552 - 25569 = 70983\n"
-               "98730 - 3789 = 94941\n"
-               "99441 - 14499 = 84942\n"
-               "98442 - 24489 = 73953\n"
-               "97533 - 33579 = 63954\n"
-               "96543 - 34569 = 61974\n"
-               "97641 - 14679 = 82962\n"
-               "98622 - 22689 = 75933\n"
-               "97533 - 33579 = 63954\n"
-               "Chain length 10\n\n");
+)");
 
-  EXPECT_STREQ(SolveByCaptureStdout(1762893).c_str(),
-               "Original number was 1762893\n"
-               "9876321 - 1236789 = 8639532\n"
-               "9865332 - 2335689 = 7529643\n"
-               "9765432 - 2345679 = 7419753\n"
-               "9775431 - 1345779 = 8429652\n"
-               "9865422 - 2245689 = 7619733\n"
-               "9776331 - 1336779 = 8439552\n"
-               "9855432 - 2345589 = 7509843\n"
-               "9875430 - 345789 = 9529641\n"
-               "9965421 - 1245699 = 8719722\n"
-               "9877221 - 1227789 = 8649432\n"
-               "9864432 - 2344689 = 7519743\n"
-               "9775431 - 1345779 = 8429652\n"
-               "Chain length 12\n\n");
+  EXPECT_STREQ(SolveByCaptureStdout(444).c_str(), R"(
+Original number was 444
+444 - 444 = 0
+0 - 0 = 0
+Chain length 2
+
+)");
+
+  EXPECT_STREQ(SolveByCaptureStdout(87543).c_str(), R"(
+Original number was 87543
+87543 - 34578 = 52965
+96552 - 25569 = 70983
+98730 - 3789 = 94941
+99441 - 14499 = 84942
+98442 - 24489 = 73953
+97533 - 33579 = 63954
+96543 - 34569 = 61974
+97641 - 14679 = 82962
+98622 - 22689 = 75933
+97533 - 33579 = 63954
+Chain length 10
+
+)");
+
+  EXPECT_STREQ(SolveByCaptureStdout(1762893).c_str(), R"(
+Original number was 1762893
+9876321 - 1236789 = 8639532
+9865332 - 2335689 = 7529643
+9765432 - 2345679 = 7419753
+9775431 - 1345779 = 8429652
+9865422 - 2245689 = 7619733
+9776331 - 1336779 = 8439552
+9855432 - 2345589 = 7509843
+9875430 - 345789 = 9529641
+9965421 - 1245699 = 8719722
+9877221 - 1227789 = 8649432
+9864432 - 2344689 = 7519743
+9775431 - 1345779 = 8429652
+Chain length 12
+
+)");
 }
