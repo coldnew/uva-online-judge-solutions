c60ff9bac63937c6052bdebe105ac4d1682f434d
Yen-Chin, Lee <coldnew.tw@gmail.com>
2022-05-31 13:58:34 +0800

    uva131

--- volume001/131/uva131.cpp
+++ volume001/131/uva131.cpp
@@ -10,15 +10,27 @@ using namespace std;
 
 // https://www.wikiwand.com/zh/%E6%92%B2%E5%85%8B%E7%89%8C%E5%9E%8B
 //
-// 1.straight-flush：同花順，牌面為T（10） - A，這裡不論花色是否相同
-// 2.four-of-a-kind：鐵支，牌面有4個相同的值
-// 3.full-house：葫蘆，牌面有3個相同值，剩下2個也相同值
-// 4.flush：同花，五張牌的花色相同，不是同花順
+// 1.highest-card：散牌，沒有以上牌型。
+// 2.one-pair：對子，牌面有一個對子，即2個同值
+// 3.two-pairs：兩對，牌面有2個對子
+// 4.three-of-a-kind：三條，牌面有3個相同的值
 // 5.straight：順子，五張牌的值連續，A可以作為1也可以作為14
-// 6.three-of-a-kind：三條，牌面有3個相同的值
-// 7.two-pairs：兩對，牌面有2個對子
-// 8.one-pair：對子，牌面有一個對子，即2個同值
-// 9.highest-card：散牌，沒有以上牌型。
+// 6.flush：同花，五張牌的花色相同，不是同花順
+// 7.full-house：葫蘆，牌面有3個相同值，剩下2個也相同值
+// 8.four-of-a-kind：鐵支，牌面有4個相同的值
+// 9.straight-flush：同花順，牌面為T（10） - A，這裡不論花色是否相同
+
+enum PokerType {
+  HighestCard = 1,
+  OnePair = 2,
+  TwoPairs = 3,
+  ThreeOfAKind = 4,
+  Straight = 5,
+  Flush = 6,
+  FullHouse = 7,
+  FourOfAKind = 8,
+  StraightFlush = 9,
+};
 
 class Card {
  public:
@@ -55,9 +67,16 @@ class Card {
 };
 
 int cur;
-Card hand[5], deck[5], sum[1 << 5][5], temp[5];
+Card hand[5], deck[5], sum[32][5], temp[5];
 
-void structure(int X, int cur1, int cur2, Card* hand, Card* deck) {
+// 32 =
+// C(5, 0) = 1
+// C(5, 1) = 5
+// C(5, 2) = 10
+// C(5, 3) = 10
+// C(5, 4) = 5
+// C(5, 5) = 1
+void GeneratePockers(int X, int cur1, int cur2, Card* hand, Card* deck) {
   if (X == 0) {
     for (int i = cur2; i < 5; i++)
       temp[i] = deck[i - cur2];
@@ -67,7 +86,7 @@ void structure(int X, int cur1, int cur2, Card* hand, Card* deck) {
 
   for (int i = cur1; i <= 5 - X; i++) {
     temp[cur2] = hand[i];
-    structure(X - 1, i + 1, cur2 + 1, hand, deck);
+    GeneratePockers(X - 1, i + 1, cur2 + 1, hand, deck);
   }
 }
 
@@ -82,74 +101,74 @@ bool IsStraight(Card cards[]) {
   return false;
 }
 
-int Judge(Card* temp) {
-  int flush = 1;
+bool IsFlush(Card cards[]) {
+  for (int i = 1; i < 5; i++)
+    if (cards[i].suit != cards[0].suit)
+      return false;
+  return true;
+}
 
+int Judge(Card* temp) {
   std::sort(temp, temp + 5);
 
   bool straight = IsStraight(temp);
-
-  for (int i = 1; i < 5; i++)
-    if (temp[i].suit != temp[0].suit) {
-      flush = 0;
-      break;
-    }
+  bool flush = IsFlush(temp);
 
   if (flush && straight)
-    return 9;
+    return StraightFlush;
 
-  int cnt[14] = {0}, four = 0, three = 0, two = 0;
+  int count[14] = {0}, four = 0, three = 0, two = 0;
 
   for (int i = 0; i < 5; i++)
-    cnt[temp[i].point]++;
+    count[temp[i].point]++;
 
   for (int i = 1; i <= 13; i++)
-    if (cnt[i] > 1) {
-      if (cnt[i] == 2)
+    if (count[i] > 1) {
+      if (count[i] == 2)
         two++;
-      else if (cnt[i] == 3)
+      else if (count[i] == 3)
         three++;
       else
         four++;
     }
 
   if (four)
-    return 8;
+    return FourOfAKind;
   if (three && two)
-    return 7;
+    return FullHouse;
   if (flush)
-    return 6;
+    return Flush;
   if (straight)
-    return 5;
+    return Straight;
   if (three)
-    return 4;
+    return ThreeOfAKind;
   if (two == 2)
-    return 3;
+    return TwoPairs;
   if (two)
-    return 2;
+    return OnePair;
 
-  return 1;
+  return HighestCard;
 }
 
-std::string ToBestHandStr(int best) {
+std::string ToPokerTypeStr(int best) {
   switch (best) {
-    case 1:
+    case HighestCard:
       return "highest-card";
-    case 2:
+    case OnePair:
       return "one-pair";
-    case 3:
+    case TwoPairs:
       return "two-pairs";
-    case 4:
+    case ThreeOfAKind:
       return "three-of-a-kind";
-    case 5:
+    case Straight:
       return "straight";
-    case 6:
+    case Flush:
       return "flush";
-    case 7:
+    case FullHouse:
       return "full-house";
-    case 8:
+    case FourOfAKind:
       return "four-of-a-kind";
-    case 9:
+    case StraightFlush:
       return "straight-flush";
   }
   return "";  // FIXME: should throw error here
@@ -179,10 +198,10 @@ void Solution(std::istream& cin) {
     cur = 0;
 
     for (int i = 0; i <= 5; i++)
-      structure(i, 0, 0, hand, deck);
+      GeneratePockers(i, 0, 0, hand, deck);
 
     int best = 0;
-    for (int i = 0; i < (1 << 5); i++) {
+    for (int i = 0; i < 32; i++) {
       int t = Judge(sum[i]);
       if (t > best)
         best = t;
@@ -191,7 +210,7 @@ void Solution(std::istream& cin) {
         break;
     }
 
-    std::cout << "Best hand: " << ToBestHandStr(best) << std::endl;
+    std::cout << "Best hand: " << ToPokerTypeStr(best) << std::endl;
   }
 }
 
